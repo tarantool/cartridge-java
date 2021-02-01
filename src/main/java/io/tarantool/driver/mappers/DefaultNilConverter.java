dada647086402a45d6f13fcb8a73d21264a77f9a
artyom dubinin <artyom.dubinin@corp.mail.ru>
2021-02-01 21:52:09 +0300

    NPE in toValue with null input

--- src/main/java/io/tarantool/driver/mappers/DefaultNilConverter.java
+++ src/main/java/io/tarantool/driver/mappers/DefaultNilConverter.java
@@ -1,17 +1,23 @@
 package io.tarantool.driver.mappers;
 
 import org.msgpack.value.NilValue;
+import org.msgpack.value.ValueFactory;
 
 /**
  * Default {@link org.msgpack.value.NilValue} to null converter
  *
  * @author Sergey Volgin
  */
-public class DefaultNilConverter implements ValueConverter<NilValue, Object> {
+public class DefaultNilConverter implements ValueConverter<NilValue, Object>, ObjectConverter<Object, NilValue> {
 
     @Override
     public Object fromValue(NilValue value) {
         return null;
+    }
+
+    @Override
+    public NilValue toValue(Object o) {
+        return ValueFactory.newNil();
     }
 
     @Override
