e869b7010234cf0acee4ed81fea651eaf06cf594
Aleksey Kuzin <akudiyar@gmail.com>
2021-02-27 12:47:52 +0300

    Add delay to AttemptsBoundRetryPolicy

--- src/main/java/io/tarantool/driver/TarantoolRequestRetryPolicies.java
+++ src/main/java/io/tarantool/driver/TarantoolRequestRetryPolicies.java
@@ -1,20 +1,25 @@
 package io.tarantool.driver;
 
+import io.tarantool.driver.exceptions.TarantoolClientException;
+import io.tarantool.driver.utils.Assert;
+
 import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
 
 /**
- * Class-container for default kinds of request retry policies
+ * Class-container for built-in request retry policies
  *
  * @author Alexey Kuzin
  */
 public final class TarantoolRequestRetryPolicies {
 
+    private static final Function<Throwable, Boolean> retryAll = t -> true;
+
     private TarantoolRequestRetryPolicies() {
     }
 
     /**
-     * Retry policy shich accepts a maximum number of attempts and an exception checking callback.
+     * Retry policy that accepts a maximum number of attempts and an exception checking callback.
      * If the exception check passes and there are any attempts left, the policy returns {@code true}.
      *
      * @param <T> exception checking callback function type
@@ -23,7 +28,8 @@ public final class TarantoolRequestRetryPolicies {
             implements RequestRetryPolicy {
 
         private int attempts;
-        private long timeout = TimeUnit.HOURS.toMillis(1);
+        private final long timeout; //ms
+        private final long delay; //ms
         private final T callback;
 
         @Override
@@ -32,33 +38,36 @@ public final class TarantoolRequestRetryPolicies {
         }
 
         /**
-         * Basic constructor
-         *
-         * @param attempts maximum number of retry attempts
-         * @param callback function checking whether the given exception may be retried
-         */
-        public AttemptsBoundRetryPolicy(int attempts, T callback) {
-            this.attempts = attempts;
-            this.callback = callback;
-        }
-
-        /**
          * Basic constructor with timeout
          *
          * @param attempts  maximum number of retry attempts
          * @param timeout   timeout for one retry attempt, in milliseconds
+         * @param delay     delay between attempts, in milliseconds
          * @param callback  function checking whether the given exception may be retried
          */
-        public AttemptsBoundRetryPolicy(int attempts, long timeout, T callback) {
+        public AttemptsBoundRetryPolicy(int attempts, long timeout, long delay, T callback) {
+            Assert.state(attempts >= 0, "Attempts must be greater or equal than 0!");
+            Assert.state(timeout >= 0, "Timeout must be greater or equal than 0!");
+            Assert.state(delay >= 0, "Timeout must be greater or equal than 0!");
+            Assert.notNull(callback, "Callback must not be null!");
+
             this.attempts = attempts;
-            this.callback = callback;
             this.timeout = timeout;
+            this.delay = delay;
+            this.callback = callback;
         }
 
         @Override
         public boolean canRetryRequest(Throwable throwable) {
             if (callback.apply(throwable) && attempts > 0) {
                 attempts--;
+                if (delay > 0) {
+                    try {
+                        TimeUnit.MILLISECONDS.sleep(delay);
+                    } catch (InterruptedException e) {
+                        throw new TarantoolClientException("Request retry delay has been interrupted");
+                    }
+                }
                 return true;
             }
             return false;
@@ -74,66 +83,120 @@ public final class TarantoolRequestRetryPolicies {
             implements RequestRetryPolicyFactory {
 
         private final int numberOfAttempts;
-        private long timeout = TimeUnit.HOURS.toMillis(1) ;
         private final T callback;
+        private final long delay; //ms
+        private final long timeout; //ms
 
         /**
-         * Basic constructor
+         * Basic constructor with timeout and delay
          *
          * @param numberOfAttempts  maximum number of retry attempts
+         * @param timeout           timeout for one retry attempt, in milliseconds
+         * @param delay             delay between retry attempts, in milliseconds
          * @param callback          function checking whether the given exception may be retried
          */
-        public AttemptsBoundRetryPolicyFactory(int numberOfAttempts, T callback) {
+        public AttemptsBoundRetryPolicyFactory(int numberOfAttempts, long timeout, long delay, T callback) {
             this.numberOfAttempts = numberOfAttempts;
+            this.timeout = timeout;
+            this.delay = delay;
             this.callback = callback;
         }
 
         /**
-         * Basic constructor with timeout
+         * Create a builder for this factory
          *
-         * @param numberOfAttempts  maximum number of retry attempts
-         * @param timeout           timeout for one retry attempt, in milliseconds
-         * @param callback          function checking whether the given exception may be retried
+         * @param attempts  maximum number of attempts
+         * @param callback  function checking whether the given exception may be retried
+         * @param <T>       exception checking callback function type
+         * @return new builder instance
          */
-        public AttemptsBoundRetryPolicyFactory(int numberOfAttempts, long timeout, T callback) {
-            this.numberOfAttempts = numberOfAttempts;
-            this.timeout = timeout;
-            this.callback = callback;
+        public static <T extends Function<Throwable, Boolean>> Builder<T> builder(int attempts, T callback) {
+            return new Builder<>(attempts, callback);
         }
 
         @Override
         public RequestRetryPolicy create() {
-            return new AttemptsBoundRetryPolicy<>(numberOfAttempts, timeout, callback);
+            return new AttemptsBoundRetryPolicy<>(numberOfAttempts, timeout, delay, callback);
         }
+
+        /**
+         * Builder for {@link AttemptsBoundRetryPolicyFactory}
+         *
+         * @param <T> exception checking callback function type
+         */
+        public static class Builder<T extends Function<Throwable, Boolean>> {
+            private final int numberOfAttempts;
+            private long timeout = TimeUnit.HOURS.toMillis(1); //ms
+            private long delay = 0; //ms
+            private final T callback;
+
+            /**
+             * Basic constructor
+             *
+             * @param numberOfAttempts  maximum number of retry attempts
+             * @param callback          function checking whether the given exception may be retried
+             */
+            public Builder(int numberOfAttempts, T callback) {
+                this.numberOfAttempts = numberOfAttempts;
+                this.callback = callback;
+            }
+
+            /**
+             * Set timeout for each attempt
+             *
+             * @param timeout   task timeout, in milliseconds
+             * @return this builder instance
+             */
+            public Builder<T> withTimeout(long timeout) {
+                this.timeout = timeout;
+                return this;
+            }
+
+            /**
+             * Set delay between attempts
+             *
+             * @param delay   task delay, in milliseconds
+             * @return this builder instance
+             */
+            public Builder<T> withDelay(long delay) {
+                this.delay = delay;
+                return this;
+            }
+
+            /**
+             * Create new factory instance
+             *
+             * @return new factory instance
+             */
+            public AttemptsBoundRetryPolicyFactory<T> build() {
+                return new AttemptsBoundRetryPolicyFactory<>(numberOfAttempts, timeout, delay, callback);
+            }
+        }
+    }
+
+    /**
+     * Create a factory for retry policy bound by retry attempts. The retry will be performed on any exception.
+     *
+     * @param numberOfAttempts  maximum number of retries, zero value means no retries
+     * @return new factory instance
+     */
+    public static
+    AttemptsBoundRetryPolicyFactory.Builder<Function<Throwable, Boolean>>
+    byNumberOfAttempts(int numberOfAttempts) {
+        return AttemptsBoundRetryPolicyFactory.builder(numberOfAttempts, retryAll);
     }
 
     /**
      * Create a factory for retry policy bound by retry attempts
      *
-     * @param numberOfAttempts  maximum number of retries
+     * @param numberOfAttempts  maximum number of retries, zero value means no retries
      * @param exceptionCheck    function callback, checking the given exception whether the request may be retried
      * @param <T> exception checking callback function type
      * @return new factory instance
      */
     public static
-    <T extends Function<Throwable, Boolean>> AttemptsBoundRetryPolicyFactory<T>
+    <T extends Function<Throwable, Boolean>> AttemptsBoundRetryPolicyFactory.Builder<T>
     byNumberOfAttempts(int numberOfAttempts, T exceptionCheck) {
-        return new AttemptsBoundRetryPolicyFactory<>(numberOfAttempts, exceptionCheck);
-    }
-
-    /**
-     * Create a factory for retry policy bound by retry attempts. Limit time for each attempt with the given timeout
-     * value
-     *
-     * @param numberOfAttempts  maximum number of retries
-     * @param timeout           timeout for one retry attempt, in milliseconds
-     * @param exceptionCheck    function callback, checking the given exception whether the request may be retried
-     * @param <T> exception checking callback function type
-     * @return new factory instance
-     */
-    public static
-    <T extends Function<Throwable, Boolean>> AttemptsBoundRetryPolicyFactory<T>
-    byNumberOfAttempts(int numberOfAttempts, long timeout, T exceptionCheck) {
-        return new AttemptsBoundRetryPolicyFactory<>(numberOfAttempts, timeout, exceptionCheck);
+        return AttemptsBoundRetryPolicyFactory.builder(numberOfAttempts, exceptionCheck);
     }
 }
