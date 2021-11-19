4e4cf521327f4946009229b67fd23c9ba05c0561
Artyom Dubinin <rusartdub@gmail.com>
2021-11-19 16:59:01 +0300

    Exception for not exist field name
    We use update/upsert from TarantoolSpace, which updates the space via
    iproto. To specify the field to update, use fieldName. Previously, if we
    made a mistake in the name, no error occurred and a request was sent to
    tarantool where fieldIndex = -1, and this is correct for tarantool
    because it uses reverse index, so -1 is the last element in the Tuple.
    For now, we'll throw a TarantoolSpaceFieldNotFoundException if the
    specified fieldName doesn't exist.
    
    Closes #63
    
    Co-authored-by: Vladimir Rogach <v.rogach@corp.mail.ru>
    Co-authored-by: Oleg Kuznetsov <oleg.kuznetsov@corp.mail.ru>

--- src/main/java/io/tarantool/driver/protocol/TarantoolRequest.java
+++ src/main/java/io/tarantool/driver/protocol/TarantoolRequest.java
@@ -12,20 +12,22 @@ import java.util.function.Supplier;
 /**
  * Base class for all kinds of requests to Tarantool server.
  * See <a href="https://www.tarantool.io/en/doc/2.3/dev_guide/internals/box_protocol/#binary-protocol-requests">
- *     https://www.tarantool.io/en/doc/2.3/dev_guide/internals/box_protocol/#binary-protocol-requests</a>
+ * https://www.tarantool.io/en/doc/2.3/dev_guide/internals/box_protocol/#binary-protocol-requests</a>
  *
  * @author Alexey Kuzin
  */
 public class TarantoolRequest {
 
-    private static AtomicLong syncId = new AtomicLong(0);
-    private static Supplier<Long> syncIdSupplier = () -> syncId.updateAndGet(n -> (n >= Long.MAX_VALUE) ? 1 : n + 1);
+    private static final AtomicLong syncId = new AtomicLong(0);
+    private static final Supplier<Long> syncIdSupplier =
+            () -> syncId.updateAndGet(n -> (n >= Long.MAX_VALUE) ? 1 : n + 1);
 
-    private TarantoolHeader header;
-    private TarantoolRequestBody body;
+    private final TarantoolHeader header;
+    private final TarantoolRequestBody body;
 
     /**
      * Basic constructor. Sets an auto-incremented request ID into the Tarantool packet header.
+     *
      * @param type request type code supported by Tarantool
      * @param body request body, may be empty
      * @see TarantoolRequestType
@@ -37,6 +39,7 @@ public class TarantoolRequest {
 
     /**
      * Get header
+     *
      * @return header instance
      */
     public TarantoolHeader getHeader() {
@@ -45,6 +48,7 @@ public class TarantoolRequest {
 
     /**
      * Get body
+     *
      * @return instance of a {@link Packable}
      */
     public Packable getBody() {
@@ -53,6 +57,7 @@ public class TarantoolRequest {
 
     /**
      * Encode incapsulated data using {@link MessagePacker}
+     *
      * @param packer configured {@link MessagePacker}
      * @param mapper object-to-entity mapper
      * @throws TarantoolDecoderException if encoding failed
