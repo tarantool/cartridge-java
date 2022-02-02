a2daa3b793e3eee968c4953eb85b45232938d4e4
Oleg Kuznetsov <oleg.kuznetsov@corp.mail.ru>
2022-02-02 16:25:43 +0300

    Add docs for TarantoolNoSuchProcedureException

--- src/main/java/io/tarantool/driver/exceptions/TarantoolNoSuchProcedureException.java
+++ src/main/java/io/tarantool/driver/exceptions/TarantoolNoSuchProcedureException.java
@@ -1,5 +1,13 @@
 package io.tarantool.driver.exceptions;
 
+/**
+ * An exception occurs when the procedure has not (yet) been defined.
+ * <p></p>
+ * For example: the tarantool/crud role may not have time to rise with the necessary functions.
+ * You can use this exception as a sign that it's worth trying a few more times until the role rises.
+ *
+ * @author Oleg Kuznetsov
+ */
 public class TarantoolNoSuchProcedureException extends TarantoolException {
     public TarantoolNoSuchProcedureException(String errorMessage) {
         super(errorMessage);
