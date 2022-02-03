217697db8eeb8399debad55c22920978ad86c6dc
Oleg Kuznetsov <71344093+wey1and@users.noreply.github.com>
2022-02-04 01:56:12 +0300

    Apply suggestions from code review
    Co-authored-by: Vladimir Rogach <73178536+vrogach2020@users.noreply.github.com>

--- src/main/java/io/tarantool/driver/exceptions/TarantoolNoSuchProcedureException.java
+++ src/main/java/io/tarantool/driver/exceptions/TarantoolNoSuchProcedureException.java
@@ -3,8 +3,8 @@ package io.tarantool.driver.exceptions;
 /**
  * An exception occurs when the procedure has not (yet) been defined.
  * <p></p>
- * For example: the tarantool/crud role may not have time to rise with the necessary functions.
- * You can use this exception as a sign that it's worth trying a few more times until the role rises.
+ * For example: this exception can be raised when a role is not up yet or being reloaded at the moment. This can happen when the tarantool 'router' node is restarted or hot-reloaded but the cluster is under load at the moment. If CRUD module is used in the cluster then 'cartridge.roles.crud-router' may have not been initialized yet and all requests to CRUD will fail with this exception.
+ * You can use this exception as a sign that it's worth trying a few more times until the role is up.
  *
  * @author Oleg Kuznetsov
  */
