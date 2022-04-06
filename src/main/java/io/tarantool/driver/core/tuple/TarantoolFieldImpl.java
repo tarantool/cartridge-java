a3ba7bf0c3dd0808ea2a97a8286227a3bf4568fd
Artyom Dubinin <rusartdub@gmail.com>
2022-04-06 22:32:14 +0300

    Cache objects in TarantoolField
    Closes #198

--- src/main/java/io/tarantool/driver/core/tuple/TarantoolFieldImpl.java
+++ src/main/java/io/tarantool/driver/core/tuple/TarantoolFieldImpl.java
@@ -3,19 +3,27 @@ package io.tarantool.driver.core.tuple;
 import io.tarantool.driver.api.tuple.TarantoolField;
 import io.tarantool.driver.mappers.MessagePackObjectMapper;
 import io.tarantool.driver.mappers.MessagePackValueMapper;
+import io.tarantool.driver.mappers.ValueConverter;
 import org.msgpack.value.Value;
 import org.msgpack.value.ValueFactory;
 
 import java.util.Objects;
+import java.util.Optional;
 
 /**
  * Basic tuple field implementation
  *
  * @author Alexey Kuzin
+ * @author Artyom Dubinin
  */
 public class TarantoolFieldImpl implements TarantoolField {
 
-    private Object value;
+    private final Object value;
+
+    private Optional<Object> convertedValueWithoutTargetType = Optional.empty();
+
+    private Optional<Class<?>> lastConvertedTargetClass = Optional.empty();
+    private Object lastConvertedValue;
 
     /**
      * Deserializing constructor
@@ -56,8 +64,14 @@ public class TarantoolFieldImpl implements TarantoolField {
         if (value == null) {
             return null;
         }
+        if (lastConvertedTargetClass.isPresent() && lastConvertedTargetClass.get().equals(targetClass)) {
+            return (O) lastConvertedValue;
+        }
         if (value instanceof Value) {
-            return mapper.fromValue((Value) value, targetClass);
+            O convertedValue = mapper.fromValue((Value) value, targetClass);
+            lastConvertedTargetClass = Optional.of(targetClass);
+            lastConvertedValue = convertedValue;
+            return convertedValue;
         }
         if (value.getClass().isAssignableFrom(targetClass)) {
             return (O) value;
@@ -68,18 +82,37 @@ public class TarantoolFieldImpl implements TarantoolField {
 
     @Override
     public Object getValue(MessagePackValueMapper mapper) {
+        if (convertedValueWithoutTargetType.isPresent()) {
+            return convertedValueWithoutTargetType.get();
+        }
         if (value instanceof Value) {
-            return mapper.fromValue((Value) value);
+            Object convertedValue = mapper.fromValue((Value) value);
+            convertedValueWithoutTargetType = Optional.of(convertedValue);
+            return convertedValue;
         }
         return value;
     }
 
     @Override
     public boolean canConvertValue(Class<?> targetClass, MessagePackValueMapper mapper) {
-        if (value instanceof Value) {
-            return mapper.getValueConverter(((Value) value).getClass(), targetClass).isPresent();
+        return canConvertValueAndCacheResult(((Value) value).getClass(), targetClass, mapper);
+    }
+
+    public <V extends Value> boolean
+    canConvertValueAndCacheResult(Class<V> entityClass, Class<?> targetClass, MessagePackValueMapper mapper) {
+        if (lastConvertedTargetClass.isPresent() && lastConvertedTargetClass.get().equals(targetClass)) {
+            return true;
         }
-        return true;
+        if (value instanceof Value) {
+            Optional<? extends ValueConverter<V, ?>> converter = mapper.getValueConverter(entityClass, targetClass);
+            if (converter.isPresent()) {
+                Object convertedValue = converter.get().fromValue((V) value);
+                lastConvertedTargetClass = Optional.of(targetClass);
+                lastConvertedValue = convertedValue;
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
