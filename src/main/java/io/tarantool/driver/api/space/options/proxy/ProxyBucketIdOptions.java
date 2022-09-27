a10a3bab53ad0077059a56c7bae4eb87626aa0bd
Artyom Dubinin <rusartdub@gmail.com>
2022-09-27 15:45:47 +0300

    Fix javadoc notes

--- src/main/java/io/tarantool/driver/api/space/options/proxy/ProxyBucketIdOptions.java
+++ src/main/java/io/tarantool/driver/api/space/options/proxy/ProxyBucketIdOptions.java
@@ -16,15 +16,16 @@ abstract class ProxyBucketIdOptions<B extends ProxyBucketIdOptions<B>> extends P
     public static final String BUCKET_ID = "bucket_id";
 
     /**
-     * Specifies bucket id for operation to perform it on storage with this bucket.
-     * By default, crud extracts a priority key from a tuple, processes it through strcrc32 hash function
-     * and gets remainder after dividing by the number of buckets.
-     * You can use your own implementation of sharding function, but you don't forget about the number of buckets,
-     * that you can get by {@code vshard.router.bucket_count} method.
+     * Specifies bucket id for an operation to perform it on storage with this bucket. It may be useful
+     * if a non-default sharding function is used or in other specific cases.
+     * By default, crud extracts primary key from a tuple and calculates a strcrc32 hash function value
+     * and gets the remainder after dividing the value by the number of buckets.
+     * You can use your own implementation of the sharding function. For retrieving the number of buckets
+     * the {@code vshard.router.bucket_count} API method can be used.
      *
-     * @param bucketId list of string field names
+     * @param bucketId number determining the location in the cluster
      * @return this options instance
-     * @see <a href="https://github.com/tarantool/vshard">vshard</a>
+     * @see <a href="https://github.com/tarantool/vshard#sharding-architecture">vshard</a>
      * @see <a href="https://github.com/tarantool/crud">crud</a>
      */
     public B withBucketId(Integer bucketId) {
