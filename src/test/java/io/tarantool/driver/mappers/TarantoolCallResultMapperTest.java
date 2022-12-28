5102be4034aef09147e7b68ff727e416ea4a7103
Alexey Kuzin <akudiyar@gmail.com>
2022-12-28 12:01:45 -0500

    Add crud batch errors handling to the single result parser

--- src/test/java/io/tarantool/driver/mappers/TarantoolCallResultMapperTest.java
+++ src/test/java/io/tarantool/driver/mappers/TarantoolCallResultMapperTest.java
@@ -5,10 +5,10 @@ import io.tarantool.driver.api.SingleValueCallResult;
 import io.tarantool.driver.api.TarantoolResult;
 import io.tarantool.driver.api.tuple.TarantoolTuple;
 import io.tarantool.driver.core.tuple.TarantoolTupleImpl;
+import io.tarantool.driver.exceptions.TarantoolFunctionCallException;
 import io.tarantool.driver.exceptions.TarantoolInternalException;
 import io.tarantool.driver.exceptions.TarantoolTupleConversionException;
 import io.tarantool.driver.mappers.factories.DefaultMessagePackMapperFactory;
-import io.tarantool.driver.mappers.factories.ResultMapperFactoryFactoryImpl;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.msgpack.value.ArrayValue;
@@ -48,17 +48,9 @@ public class TarantoolCallResultMapperTest {
 
     @Test
     void testSingleValueCallResultMapper() {
-        MessagePackMapper defaultMapper = DefaultMessagePackMapperFactory.getInstance().defaultComplexTypesMapper();
-        TarantoolTupleResultMapperFactory tarantoolTupleResultMapperFactory =
-            TarantoolTupleResultMapperFactoryImpl.getInstance();
-        CallResultMapper<TarantoolResult<TarantoolTuple>,
-            SingleValueCallResult<TarantoolResult<TarantoolTuple>>> mapper =
-            tarantoolTupleResultMapperFactory
-                .withSingleValueArrayToTarantoolTupleResultMapper(defaultMapper, null);
-
         //[nil, message]
         ArrayValue errorResult = ValueFactory.newArray(ValueFactory.newNil(), ValueFactory.newString("ERROR"));
-        assertThrows(TarantoolInternalException.class, () -> mapper.fromValue(errorResult));
+        assertThrows(TarantoolInternalException.class, () -> defaultResultMapper.fromValue(errorResult));
 
         //[nil, {str=message, stack=stacktrace}]
         MapValue error = ValueFactory.newMap(
@@ -68,9 +60,9 @@ public class TarantoolCallResultMapperTest {
             ValueFactory.newString("stacktrace")
         );
         ArrayValue errorResult1 = ValueFactory.newArray(ValueFactory.newNil(), error);
-        assertThrows(TarantoolInternalException.class, () -> mapper.fromValue(errorResult1));
+        assertThrows(TarantoolInternalException.class, () -> defaultResultMapper.fromValue(errorResult1));
 
-        //[[[],...]]
+        //[[[], ...]]
         List<Object> nestedList1 = Arrays.asList("nested", "array", 1);
         TarantoolTuple tupleOne = new TarantoolTupleImpl(Arrays.asList("abc", 1234, nestedList1), defaultMapper);
         List<Object> nestedList2 = Arrays.asList("nested", "array", 2);
@@ -78,7 +70,7 @@ public class TarantoolCallResultMapperTest {
         ArrayValue testTuples = ValueFactory.newArray(
             tupleOne.toMessagePackValue(defaultMapper), tupleTwo.toMessagePackValue(defaultMapper));
         ArrayValue callResult = ValueFactory.newArray(testTuples);
-        SingleValueCallResult<TarantoolResult<TarantoolTuple>> result = mapper.fromValue(callResult);
+        SingleValueCallResult<TarantoolResult<TarantoolTuple>> result = defaultResultMapper.fromValue(callResult);
         TarantoolResult<TarantoolTuple> tuples = result.value();
 
         assertEquals(2, tuples.size());
@@ -88,21 +80,47 @@ public class TarantoolCallResultMapperTest {
         assertEquals("def", tuples.get(1).getString(0));
         assertEquals(5678, tuples.get(1).getInteger(1));
         assertEquals(nestedList2, tuples.get(1).getList(2));
+
+        //[[[], ...], [message]]
+        ArrayValue errorResult2 =
+            ValueFactory.newArray(testTuples, ValueFactory.newArray(ValueFactory.newString("ERROR")));
+        assertThrows(TarantoolInternalException.class, () -> defaultResultMapper.fromValue(errorResult2));
+
+        //[[[], ...], [{str=message, stack=stacktrace}, {str=message, stack=stacktrace}]]
+        MapValue error1 = ValueFactory.newMap(error.asMapValue().map());
+        ArrayValue errors = ValueFactory.newArray(error, error1);
+        ArrayValue errorResult3 = ValueFactory.newArray(testTuples, errors);
+        assertThrows(TarantoolInternalException.class, () -> defaultResultMapper.fromValue(errorResult3));
+    }
+
+    @Test
+    void testDefaultTarantoolTupleResponse_singleResultShouldThrowException_cornerCase() {
+        ArrayValue testTuples = ValueFactory.newArray(
+            tupleOne.toMessagePackValue(defaultMapper), tupleTwo.toMessagePackValue(defaultMapper));
+
+        // The second value is a MsgPack array, and it is parsed an error. However, the result format is
+        // actually wrong, so the exact error contents does not matter in this case.
+        assertThrows(TarantoolInternalException.class, () -> defaultResultMapper.fromValue(testTuples), "def");
     }
 
     @Test
     void testDefaultTarantoolTupleResponse_singleResultShouldThrowException() {
-        ArrayValue testTuples = ValueFactory.newArray(
-            tupleOne.toMessagePackValue(defaultMapper), tupleTwo.toMessagePackValue(defaultMapper));
+        ArrayValue testTuples = ValueFactory.newArray(tupleOne.toMessagePackValue(defaultMapper));
 
+        // Another corner case, tuple result mapper should not be used for this result format
         assertThrows(TarantoolTupleConversionException.class, () -> defaultResultMapper.fromValue(testTuples));
     }
 
     @Test
+    void testDefaultTarantoolTupleResponse_singleResultShouldThrowException_tooManyValues() {
+        ArrayValue testTuples = ValueFactory.newArray(tupleOne.toMessagePackValue(defaultMapper),
+            tupleOne.toMessagePackValue(defaultMapper), tupleOne.toMessagePackValue(defaultMapper));
+
+        assertThrows(TarantoolFunctionCallException.class, () -> defaultResultMapper.fromValue(testTuples));
+    }
+
+    @Test
     void testMultiValueCallResultMapper() {
-        MessagePackMapper defaultMapper = DefaultMessagePackMapperFactory.getInstance().defaultComplexTypesMapper();
-        TarantoolTupleResultMapperFactory tarantoolTupleResultMapperFactory =
-            TarantoolTupleResultMapperFactoryImpl.getInstance();
         CallResultMapper<TarantoolResult<TarantoolTuple>,
             MultiValueCallResult<TarantoolTuple, TarantoolResult<TarantoolTuple>>> mapper =
             tarantoolTupleResultMapperFactory
