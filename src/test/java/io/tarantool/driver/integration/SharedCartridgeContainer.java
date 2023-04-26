c409efaabac192176fea5c935d55293f525e346f
Artyom Dubinin <rusartdub@gmail.com>
2023-04-26 14:52:43 +0300

    Remove unnecessary build args from MixedContainer

--- src/test/java/io/tarantool/driver/integration/SharedCartridgeContainer.java
+++ src/test/java/io/tarantool/driver/integration/SharedCartridgeContainer.java
@@ -1,14 +1,14 @@
 package io.tarantool.driver.integration;
 
+import java.io.IOException;
+import java.time.Duration;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testcontainers.containers.TarantoolCartridgeContainer;
 import org.testcontainers.containers.output.Slf4jLogConsumer;
 import org.testcontainers.containers.wait.strategy.Wait;
-
-import java.io.IOException;
-import java.time.Duration;
-import java.util.List;
 
 public abstract class SharedCartridgeContainer {
 
