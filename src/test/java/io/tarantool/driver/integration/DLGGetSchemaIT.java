0a23a92328f833d69362333ac4e789cfa170b00c
Ivan Dneprov <vanya.dneprov@gmail.com>
2023-06-06 18:46:02 +0300

    wip1

--- src/test/java/io/tarantool/driver/integration/DLGGetSchemaIT.java
+++ src/test/java/io/tarantool/driver/integration/DLGGetSchemaIT.java
@@ -4,10 +4,14 @@ import io.tarantool.driver.api.TarantoolClient;
 import io.tarantool.driver.api.TarantoolClientFactory;
 import io.tarantool.driver.api.TarantoolResult;
 import io.tarantool.driver.api.conditions.Conditions;
+import io.tarantool.driver.api.metadata.TarantoolIndexMetadata;
+import io.tarantool.driver.api.metadata.TarantoolMetadataContainer;
+import io.tarantool.driver.api.metadata.TarantoolMetadataProvider;
 import io.tarantool.driver.api.space.TarantoolSpaceOperations;
 import io.tarantool.driver.api.tuple.DefaultTarantoolTupleFactory;
 import io.tarantool.driver.api.tuple.TarantoolTuple;
 import io.tarantool.driver.api.tuple.TarantoolTupleFactory;
+import io.tarantool.driver.core.metadata.SpacesTarantoolMetadataContainer;
 import io.tarantool.driver.mappers.DefaultMessagePackMapperFactory;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -19,23 +23,19 @@ import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-
-/**
- * @author Ivan Dneprov
- * <p>
- * WARNING: If you updated the code in this file, don't forget to update the readme permalinks!
- */
 @Testcontainers
-public class DDLGetChemaIT {
+public class DLGGetSchemaIT {
 
-    private static final Logger log = LoggerFactory.getLogger(DDLGetChemaIT.class);
+    private static final Logger log = LoggerFactory.getLogger(DLGGetSchemaIT.class);
 
     @Container
     private static final TarantoolContainer container = new TarantoolContainer()
-        .withScriptFileName("single-instance.lua")
+        .withScriptFileName("numerical-index-name.lua")
         .withLogConsumer(new Slf4jLogConsumer(log));
 
     private static final DefaultMessagePackMapperFactory mapperFactory = DefaultMessagePackMapperFactory.getInstance();
@@ -55,20 +55,10 @@ public class DDLGetChemaIT {
     @Test
     public void simplePutAndGetTest() throws Exception {
         try (TarantoolClient<TarantoolTuple, TarantoolResult<TarantoolTuple>> client = setupClient()) {
-            TarantoolTupleFactory tupleFactory =
-                new DefaultTarantoolTupleFactory(mapperFactory.defaultComplexTypesMapper());
-            TarantoolSpaceOperations<TarantoolTuple, TarantoolResult<TarantoolTuple>> regionSpace =
-                client.space("region");
-
-            TarantoolTuple tarantoolTuple = tupleFactory.create(Arrays.asList(77, "Moscow"));
-            TarantoolResult<TarantoolTuple> insertTuples = regionSpace.insert(tarantoolTuple).get();
-            TarantoolResult<TarantoolTuple> selectTuples =
-                regionSpace.select(Conditions.equals("id", 77)).get();
-
-            TarantoolTuple insertTuple = insertTuples.get(0);
-            TarantoolTuple selectTuple = selectTuples.get(0);
-            assertEquals(insertTuple.getInteger("id"), selectTuple.getInteger("id"));
-            assertEquals(insertTuple.getString("name"), selectTuple.getString("name"));
+            TarantoolMetadataProvider metadataProvider = client.metadataProvider();
+            TarantoolMetadataContainer metadata = metadataProvider.getMetadata().join();
+            Map<String, Map<Object, TarantoolIndexMetadata>> indexMetadata = metadata.getIndexMetadataBySpaceName();
+            Map indexMap = indexMetadata.get("region");
         }
     }
 }
