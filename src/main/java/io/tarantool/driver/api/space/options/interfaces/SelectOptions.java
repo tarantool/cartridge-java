31e42212daa97007eac65c60613b5f35ead45dd2
Belonogov Nikolay <nikolai.belonogov@vk.team>
2023-09-13 16:34:57 +0300

    Change batch_size option interfaces
    - Add OperationWIthBatchSizeOptions class.
    - Change ProxySelectOptions, SelectOptions classes.
    - Add package-info.
    
    Needed for #417.

--- src/main/java/io/tarantool/driver/api/space/options/interfaces/SelectOptions.java
+++ src/main/java/io/tarantool/driver/api/space/options/interfaces/SelectOptions.java
@@ -1,11 +1,10 @@
 package io.tarantool.driver.api.space.options.interfaces;
 
+import io.tarantool.driver.api.space.options.contracts.OperationWIthBatchSizeOptions;
 import io.tarantool.driver.api.space.options.contracts.OperationWithBucketIdOptions;
 import io.tarantool.driver.api.space.options.contracts.OperationWithFieldsOptions;
 import io.tarantool.driver.api.space.options.contracts.OperationWithModeOptions;
 import io.tarantool.driver.api.space.options.contracts.OperationWithTimeoutOptions;
-
-import java.util.Optional;
 
 /**
  * Marker interface for space select operation options
@@ -15,12 +14,5 @@ import java.util.Optional;
  */
 public interface SelectOptions<T extends SelectOptions<T>>
     extends OperationWithBucketIdOptions<T>, OperationWithTimeoutOptions<T>, OperationWithFieldsOptions<T>,
-    OperationWithModeOptions<T> {
-    /**
-     * Return the internal size of batch for transferring data between
-     * storage and router nodes.
-     *
-     * @return batch size
-     */
-    Optional<Integer> getBatchSize();
+    OperationWithModeOptions<T>, OperationWIthBatchSizeOptions<T> {
 }
